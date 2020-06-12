b05b41a78124d93aae67bf3a9a85d9df8459110b
admin <admin@DESKTOP-GL1URBL>
2020-06-12 14:31:09 +0530

    Example5

---
+++ Milestone/src/com/wipro/flowcontrolstatements/Exp5.java
@@ -0,0 +1,17 @@
+package com.wipro.flowcontrolstatements;
+
+public class Exp5 {
+
+	public static void main(String[] args) {
+		int a=Integer.parseInt(args[0]);
+		if (a%2==0)
+		{
+			System.out.println("Number is Even");
+		}
+		else
+		{
+			System.out.println("Number is Odd");
+	}
+
+}
+}
