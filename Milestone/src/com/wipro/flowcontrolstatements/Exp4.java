aeeb98b5b43de39f48a7660a35539e6285b42079
admin <admin@DESKTOP-GL1URBL>
2020-06-12 14:24:56 +0530

    Example4

---
+++ Milestone/src/com/wipro/flowcontrolstatements/Exp4.java
@@ -0,0 +1,21 @@
+
+package com.wipro.flowcontrolstatements;
+public class Exp4 {
+
+	public static void main(String[] args) {
+		Double a=Double.parseDouble(args[0]);
+		if (a==0.0)
+		{
+			System.out.println("Number is zero");
+		}
+		else if(a>0.0)
+		{
+			System.out.println("Number is positive");
+		}
+		else
+		{
+			System.out.println("Number is negative");
+	}
+
+}
+}
